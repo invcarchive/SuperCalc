ee56e829391f4a37231e6578c9401d9b23403ab1
BaconBoi95 <leeland230.fun@gmail.com>
2019-05-09 13:24:40 -0400

    Update basic.cpp

--- lib/calculators/basic.cpp
+++ lib/calculators/basic.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 using namespace std;
 void basiccalc() {
-  
+  // setup the varibles
+  float usrnuminput;
+  char operation;
+  float cool;
+  float awnser;
+  // get user input
+  cin >> usrnuminput;
+  cout << endl << usrnuminput << endl;
+  cin >> operation;
+  cout << endl << usrnuminput << " " << operation << endl;
+  cin >> cool;
+  // calculate
+  cout << endl << usrnuminput << " " << operation << " " << cool << " = " << calculatethingy(usrnuminput,operation,cool) << endl;   
+};
+
+float calculatethingy(float a,char b,float c) {
+  else if(b=='+') {
+    return a+c
+  }
+  else if(b=='-') {
+    return a-c
+  }
+  else if(b=='*') {
+    return a*c
+  }
+  else if(b=='/') {
+    return a/c
+  }
+  else {
+    cout << "no";
+  };
 };
